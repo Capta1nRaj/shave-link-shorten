5f88d4adb9fd040e524a995ecfe8fa63d55828a9
Priyal Raj <priyalraj99@gmail.com>
2024-05-19 23:56:40 +0530

    /signIn & /signUp Page resendOTP not working bug fixed

--- src/app/api/EmailArmorAPIs/resendOTP/route.ts
+++ src/app/api/EmailArmorAPIs/resendOTP/route.ts
@@ -11,11 +11,11 @@ export async function POST(request: NextRequest) {
         const { userName, method } = await request.json();
 
         const cookieStore = cookies();
-        const id = cookieStore.get('id') || "";
+        let id = cookieStore.get('id')?.value || "";
 
-        if (!userAgent || (method !== "forgotPassword" && !id)) { return NextResponse.json({ message: "Internal Server Error.", status: 500 }, { status: 200 }); }
+        if (!userAgent) { return NextResponse.json({ message: "Internal Server Error.", status: 500 }, { status: 200 }); }
 
-        const response = await resendOTP(userName, method, userAgent, '', userIP);
+        const response = await resendOTP(userName, method, userAgent, id || "", userIP);
 
         if (!response) { return NextResponse.json({ message: "Internal Server Error.", status: 500 }, { status: 200 }); }
 
