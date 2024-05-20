96e9922e03aa37de0b3f65d3b93454c2c4a43122
Priyal Raj <priyalraj99@gmail.com>
2024-05-20 18:40:19 +0530

    All dark: removed from code

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -12,9 +12,9 @@ type FormData = {
     userpassword: string;
 };
 
-const labelCSS = `block mb-2 text-sm font-medium text-gray-900 dark:text-white`;
-const inputCSS = `bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`;
-const buttonCSS = `w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`;
+const labelCSS = `block mb-2 text-sm font-medium text-white`;
+const inputCSS = `bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500`;
+const buttonCSS = `w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800`;
 
 const ForgotPasswordPage = () => {
 
@@ -110,11 +110,11 @@ const ForgotPasswordPage = () => {
     return (
         <>
             {!otpScene ?
-                <section className="bg-gray-50 dark:bg-gray-900">
+                <section className="bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                     Reset you password
                                 </h1>
                                 <form className="space-y-4 md:space-y-6" onSubmit={sendOTPToUserFunction}>
@@ -128,8 +128,8 @@ const ForgotPasswordPage = () => {
                                     }
 
                                     <button type="submit" className={buttonCSS}> Send OTP </button>
-                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                        Don`t have an account yet? <Link href="/signUp" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                                    <p className="text-sm font-light text-gray-400">
+                                        Don`t have an account yet? <Link href="/signUp" className="font-medium text-primary-600 hover:underline text-primary-500">Sign up</Link>
                                     </p>
                                 </form>
                             </div>
@@ -139,11 +139,11 @@ const ForgotPasswordPage = () => {
 
                 :
 
-                <section className="bg-gray-50 dark:bg-gray-900">
+                <section className="bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                     Enter OTP & New Password
                                 </h1>
                                 <form className="space-y-4 md:space-y-6" onSubmit={updateUserPasswordFunction}>
--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -112,9 +112,9 @@ const SignUpPage = () => {
         }
     }
 
-    const labelCSS = `block mb-2 text-sm font-medium text-gray-900 dark:text-white`;
-    const inputCSS = `bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`;
-    const buttonCSS = `w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`;
+    const labelCSS = `block mb-2 text-sm font-medium text-white`;
+    const inputCSS = `bg-gray-50 border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500`;
+    const buttonCSS = `w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800`;
 
     const [loading, setLoading] = useState(true);
     useEffect(() => {
@@ -143,11 +143,11 @@ const SignUpPage = () => {
     return (
         <>
             {!otpScene ?
-                <section className="bg-gray-50 dark:bg-gray-900">
+                <section className="bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                     Create a new account
                                 </h1>
                                 <form className="space-y-4 md:space-y-6" onSubmit={signUpUserFunction}>
@@ -177,8 +177,8 @@ const SignUpPage = () => {
                                     }
 
                                     <button type="submit" className={buttonCSS}>SIGN UP</button>
-                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                        Already have an account? <Link href="/signIn" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</Link>
+                                    <p className="text-sm font-light text-gray-400">
+                                        Already have an account? <Link href="/signIn" className="font-medium text-primary-600 hover:underline text-primary-500">Sign In</Link>
                                     </p>
                                 </form>
                             </div>
@@ -188,11 +188,11 @@ const SignUpPage = () => {
 
                 :
 
-                <section className="bg-gray-50 dark:bg-gray-900">
+                <section className="bg-gray-900">
                     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                     Enter OTP
                                 </h1>
                                 <form className="space-y-4 md:space-y-6" onSubmit={signUpUserVerifyFunction}>
