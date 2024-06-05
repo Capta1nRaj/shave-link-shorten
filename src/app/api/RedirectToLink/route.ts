81c2258842b9e97d7e611a13a5014bee431b895b
Priyal Raj <priyalraj99@gmail.com>
2024-06-06 00:53:02 +0530

    Browser OS Device tracing feature added

--- src/app/api/RedirectToLink/route.ts
+++ src/app/api/RedirectToLink/route.ts
@@ -5,17 +5,20 @@ import { getMonthNumber, getWeekNumber, getYearNumber } from '@/utils/DateFuncti
 import { FetchUserIP } from '@/utils/FetchUserIP';
 import axios from 'axios';
 import { connect2MongoDB } from 'connect2mongodb';
-import { NextResponse, type NextRequest } from 'next/server';
+import { NextResponse, type NextRequest, userAgent } from 'next/server';
 
 export async function GET(request: NextRequest) {
     try {
+
+        const { browser, os, device, isBot } = userAgent(request)
+
         //! Connecting to MongoDB
         await connect2MongoDB();
 
         const searchParams = request.nextUrl.searchParams;
         const alias = searchParams.get('alias');
 
-        const data = await linksListModel.findOneAndUpdate({ alias }, { $inc: { clicksCount: 1 } }).select('userName destinationURL toSupport appOpener appType status');
+        const data = await linksListModel.findOne({ alias }).select('userName destinationURL toSupport appOpener appType status');
         if (!data) { return NextResponse.json({ message: "Link not found!", statusCode: 404 }, { status: 200 }); }
 
         // //! Get user IP Address & fetch their country
@@ -24,18 +27,38 @@ export async function GET(request: NextRequest) {
         const response = await axios.get(`http://ip-api.com/json/${ip}`);
         const { data: { query, status, country, countryCode, region, regionName, city, zip, timezone, isp, org, as } } = response;
 
-        await new clicksTrackingModel({ userName: data.userName, alias: data._id, ip: query, countryName: country, countryCode, stateCode: region, stateName: regionName, cityName: city, zip, timezone, isp, org, as }).save();
+        if (status) {
+            await new clicksTrackingModel({
+                userName: data.userName,
+                alias: data._id,
+                ip: query,
+                countryName: country,
+                countryCode,
+                stateCode: region,
+                stateName: regionName,
+                cityName: city,
+                zip,
+                timezone,
+                isp,
+                org,
+                as,
+                browser: browser.name,
+                os: os.name,
+                device: device.type || "Windows"
+            }).save();
 
-        //! Increment link click count for current week, month, & year
-        const updateWebsiteStats = await websiteStatsModel.updateOne({ weekNumber: getWeekNumber(), monthNumber: getMonthNumber(), yearNumber: getYearNumber() }, { $inc: { linksClicksCount: 1 } });
+            await linksListModel.updateMany({ alias }, { $inc: { clicksCount: 1 } });
 
-        if (updateWebsiteStats.matchedCount === 0) {
-            await new websiteStatsModel({ weekNumber: getWeekNumber(), monthNumber: getMonthNumber(), yearNumber: getYearNumber(), linksClicksCount: 1 }).save();
+            //! Increment link click count for current week, month, & year
+            const updateWebsiteStats = await websiteStatsModel.updateOne({ weekNumber: getWeekNumber(), monthNumber: getMonthNumber(), yearNumber: getYearNumber() }, { $inc: { linksClicksCount: 1 } });
+
+            if (updateWebsiteStats.matchedCount === 0) {
+                await new websiteStatsModel({ weekNumber: getWeekNumber(), monthNumber: getMonthNumber(), yearNumber: getYearNumber(), linksClicksCount: 1 }).save();
+            }
         }
 
         return NextResponse.json({ message: "Link fetched successfully.", statusCode: 200, destinationURL: data.destinationURL, toSupport: data.toSupport, appOpener: data.appOpener, status: data.status }, { status: 200 });
     } catch (error) {
-
         return NextResponse.json({ message: "Internal Server Error.", status: 500 }, { status: 200 });
     }
 }
--- src/models/clicksTrackingModel.ts
+++ src/models/clicksTrackingModel.ts
@@ -4,8 +4,10 @@
 import mongoose from "mongoose";
 
 const ClicksTrackingSchema = new mongoose.Schema({
+    //* Admin Details
     userName: { type: mongoose.Schema.Types.ObjectId, ref: 'userAccounts', required: true },
     alias: { type: mongoose.Schema.Types.ObjectId, ref: "linksList", required: true },
+    //* Viewer Location Details
     ip: { type: String },
     countryName: { type: String, default: "Unknown" },
     countryCode: { type: String, default: "Unknown" },
@@ -17,6 +19,10 @@ const ClicksTrackingSchema = new mongoose.Schema({
     isp: { type: String, default: "Unknown" },
     org: { type: String, default: "Unknown" },
     as: { type: String, default: "Unknown" },
+    //* Viewer Device Details
+    browser: { type: String, default: "Unknown" },
+    os: { type: String, default: "Unknown" },
+    device: { type: String, default: "Windows" },
 }, { timestamps: true })
 
 ClicksTrackingSchema.index({ userName: 1 })
@@ -24,6 +30,8 @@ ClicksTrackingSchema.index({ alias: 1 })
 ClicksTrackingSchema.index({ countryName: 1 })
 ClicksTrackingSchema.index({ stateName: 1 })
 ClicksTrackingSchema.index({ cityName: 1 })
-ClicksTrackingSchema.index({ timezone: 1 })
+ClicksTrackingSchema.index({ browser: 1 })
+ClicksTrackingSchema.index({ os: 1 })
+ClicksTrackingSchema.index({ device: 1 })
 
 export default mongoose.models.clicksTracking || mongoose.model('clicksTracking', ClicksTrackingSchema);
