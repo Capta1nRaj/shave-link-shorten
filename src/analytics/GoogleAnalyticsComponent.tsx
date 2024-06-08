ff4940cb0f4b6238eb5da4656f96a04482c13505
Priyal Raj <priyalraj99@gmail.com>
2024-06-08 08:58:48 +0530

    GoogleAnalytics & MicrosoftClarity code updated

--- src/analytics/GoogleAnalyticsComponent.tsx
+++ src/analytics/GoogleAnalyticsComponent.tsx
@@ -3,6 +3,6 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function GoogleAnalyticsComponent() {
   return (
-    <GoogleAnalytics gaId={`G-R5X8FW71Y1`} />
+    <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_CODE}`} />
   )
 }
--- src/analytics/MicrosoftClarityComponent.tsx
+++ src/analytics/MicrosoftClarityComponent.tsx
@@ -1,21 +1,19 @@
 'use client'
-import Script from 'next/script'
-import React from 'react'
+
+import Script from "next/script"
 
 export default function MicrosoftClarityComponent() {
     return (
-        <>
-            <Script id="microsoft-clarity-init" strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    (function(c,l,a,r,i,t,y){
-                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "maiy179s9k");
-                    `
-                }}
-            />
-        </ >
+        <Script id="microsoft-clarity-init" strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSFT_CLARITY_CODE}");
+                `,
+            }}
+        />
     )
 }
